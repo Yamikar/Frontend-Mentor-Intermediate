a19d09ce320e2b5af01eac7f28124b9bbc3e6da0
Yılmaz Keskin <104618545+Yamikar@users.noreply.github.com>
2024-01-25 23:45:57 +0300

    Add files via upload

---
+++ manage landing page/app.js
@@ -0,0 +1,18 @@
+
+/*js mobile navbar fuction */
+
+
+
+function mobile(){
+
+    document.querySelector("header").style.display="none";
+    document.querySelector("ol").style.display="none";
+    document.querySelector("ol").style.display="none";
+}
+
+function navbar(){
+
+
+}
+
+/**********************************-------------------------------------------*********************/
